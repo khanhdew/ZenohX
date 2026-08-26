60663b16c7b562a44dbec43bad926902c0993134
Khanh Nguyen <khanhdeptrai292004@gmail.com>
2026-08-26 14:49:54 +0700

    chore: add Apache-2.0 license headers and update gitignore

--- src-tauri/tests/e2e_integration_test.rs
+++ src-tauri/tests/e2e_integration_test.rs
@@ -1,3 +1,17 @@
+// Copyright 2026 ZenohX Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! End-to-end integration tests for ZenohX MVP
 //! Verifies SQLite profile persistence, concurrent peer sessions,
 //! pub/sub message propagation, and query/reply RPC roundtrips.
